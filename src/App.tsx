e7598748ca9c550438a2e8bfdee572afe5c87b4b
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2024-12-22 09:47:11 +0100

    Deploy to gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <h1>img2ral</h1>
       <div className="box">
-        {Object.keys(PALLETE).map((ral, idx) => {
+        {Object.keys(PALLETE).map((ral) => {
           const [hex, name] = PALLETE[ral]
           const color = adjustColorBrightness(hex, 0.5)
 
@@ -17,7 +17,7 @@ function App() {
               className="ral"
               style={{
                 backgroundColor: hex,
-                top: idx,
+                top: 0,
               }}
             >
               <div className="colorName" style={{ color }}>
