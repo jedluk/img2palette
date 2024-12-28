39a3a7c26b6f5452eaab3fd211f9414de9f9b681
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2024-12-28 14:09:32 +0100

    Change background

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,27 @@ import { PALLETE } from './lib/ral'
 
 function App() {
   return (
-    <>
-      <h1>img2ral</h1>
-      <div className="box">
-        {Object.keys(PALLETE).map((ral) => {
-          const [hex, name] = PALLETE[ral]
-          const color = adjustColorBrightness(hex, 0.5)
+    <div className="box">
+      {Object.keys(PALLETE).map((ral, idx) => {
+        const [hex, name] = PALLETE[ral]
+        const color = adjustColorBrightness(hex, 0.5)
 
-          return (
-            <div
-              key={ral}
-              className="ral"
-              style={{
-                backgroundColor: hex,
-                top: 0,
-              }}
-            >
-              <div className="colorName" style={{ color }}>
-                {name}
-              </div>
+        return (
+          <div
+            key={ral}
+            className="ral"
+            style={{
+              backgroundColor: hex,
+              top: idx * 1.5,
+            }}
+          >
+            <div className="colorName" style={{ color }}>
+              {name}
             </div>
-          )
-        })}
-      </div>
-    </>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
