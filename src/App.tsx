35dc2e3176e51a6f6b18c71fe208b0a5ad8df4d9
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2025-01-11 15:35:02 +0100

    Add Reset button

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Background } from './components/Background'
 import { Dropzone } from './components/Dropzone'
 import { RALPallette } from './components/RALPallette'
 import { ImageDrawer } from './components/ImageDrawer'
+import { ResetButton } from './components/ResetButton'
 
 function App() {
   const [file, setFile] = useState<File | null>(null)
@@ -11,10 +12,12 @@ function App() {
   return (
     <>
       <Background animate={file === null} />
-      {file === null ? (
-        <Dropzone onChange={setFile} />
-      ) : (
-        <ImageDrawer file={file} />
+      {file === null && <Dropzone onChange={setFile} />}
+      {file instanceof File && (
+        <>
+          <ResetButton onClick={() => setFile(null)} />
+          <ImageDrawer file={file} />
+        </>
       )}
       <RALPallette />
     </>
