c65936857b0a7bfbf11ad10c91c14da92d2a533f
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2025-01-11 15:15:02 +0100

    Refactor app

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,22 @@
+import { useState } from 'react'
 import './App.css'
-import { useEffect, useState } from 'react'
-import { adjustColorBrightness } from './lib/color'
-import { PALLETE } from './lib/ral'
 import { Background } from './Background'
 import { Dropzone } from './Dropzone'
+import { RALPallette } from './RALPallette'
+import { ImageDrawer } from './ImageDrawer'
 
 function App() {
-  const [spread, setSpread] = useState(270)
-
-  const ralColors = Object.keys(PALLETE)
-
-  const totalItems = ralColors.length
-  const angleIncrement = spread / (totalItems - 1)
-
-  useEffect(() => {
-    const l = ralColors.length
-    if (l < 5) {
-      setSpread(20)
-    } else if (l < 10) {
-      setSpread(90)
-    }
-  }, [ralColors])
+  const [file, setFile] = useState<File | null>(null)
 
   return (
     <>
-      <Background />
-      <Dropzone />
-      <div className="ral-pallette">
-        <div className="center-circle">
-          <button onClick={() => setSpread(spread + 10)}>+</button>
-          <button onClick={() => setSpread(spread - 10)}>-</button>
-        </div>
-        {ralColors.map((ral, idx) => {
-          const [hex, name] = PALLETE[ral]
-          const color = adjustColorBrightness(hex, 0.5)
-          const angle = -spread / 2 + idx * angleIncrement
-
-          return (
-            <div
-              key={ral}
-              className="ral-color"
-              style={{
-                backgroundColor: hex,
-                transform: `rotate(${angle}deg)`,
-                top: 'calc(50% + 35px)',
-                marginTop: '-75px',
-              }}
-            >
-              <div className="colorName" style={{ color }}>
-                {name} ({ral.slice(4)})
-              </div>
-            </div>
-          )
-        })}
-      </div>
+      <Background animate={file === null} />
+      {file === null ? (
+        <Dropzone onChange={setFile} />
+      ) : (
+        <ImageDrawer file={file} />
+      )}
+      <RALPallette />
     </>
   )
 }
--- src/Background.tsx
+++ src/Background.tsx
@@ -1,9 +1,15 @@
 import { PALLETE } from './lib/ral'
 import style from './Background.module.css'
+import { classNames } from './lib/classNames'
 
-export function Background() {
+export function Background(props: { animate: boolean }) {
   return (
-    <div className={style.background}>
+    <div
+      className={classNames(
+        style.background,
+        !props.animate ? style.noAnimation : ''
+      )}
+    >
       {Object.values(PALLETE).map(([hex, name]) => (
         <div
           key={name}
